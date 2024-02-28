a5dc4bfafea2eab5fb3e14954fc4d600e66e0611
squidward13 <adm.mail1997@yandex.ru>
2024-02-28 12:38:27 +0300

    Init

---
+++ Shop/shop_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+ 
+path('', views.IndexPageView.as_view(), name="index"),
+path("all_orders/<int:pk>/", views.AllOrdersView.as_view(), name="all_orders"),
+path("all_orders_products/<int:pk>/", views.AllProductsInOrdersView.as_view(), name="all_orders_products"),
+path("add_product/", views.AddNewProductView.as_view(), name="add_product"),
+path("add_product_form/", views.AddNewProductFormView.as_view(), name="add_product_form"),
+path("all_products/", views.AllProductsView.as_view(), name="all_products"),
+]
